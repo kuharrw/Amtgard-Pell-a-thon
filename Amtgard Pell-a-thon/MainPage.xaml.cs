1f9b1c9fc4356b77e6dfb88f1d269ab41c57d248
Ryan Kuhar <kuharrw@gmail.com>
2021-12-20 21:37:40 -0500

    Add project files.

---
+++ Amtgard Pell-a-thon/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Amtgard_Pell_a_thon
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
